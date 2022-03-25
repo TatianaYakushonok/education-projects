71f18aef7e8772df6b6e65fd3630b571f36251b1
TatianaYakushonok <yakush2@yandex.ru>
2022-03-26 02:04:29 +0700

    feat: add adaptiv

--- gocorona/js/main.js
+++ gocorona/js/main.js
@@ -8,3 +8,29 @@
         }
     } 
  }());
+
+ // BURGER MENU
+
+ (function(){
+    const burger = document.querySelector('.burger_menu');
+    const menuList = document.querySelector('.header_list');
+    const menuLink = document.querySelectorAll('.header_link');
+    const burgerLine = document.querySelector('.burger_menu-line');
+
+    burger.addEventListener('click', () => {
+        menuList.classList.add('header_list-vis');
+        if(burgerLine.classList.contains('close')) {
+            menuList.classList.remove('header_list-vis');
+            burgerLine.classList.remove('close');
+        } else {
+            burgerLine.classList.add('close');
+        }
+    })
+    
+    menuLink.forEach(element => {
+        element.addEventListener('click', () => {
+            menuList.classList.remove('header_list-vis');
+            burgerLine.classList.remove('close');
+        })
+    });
+ }());
